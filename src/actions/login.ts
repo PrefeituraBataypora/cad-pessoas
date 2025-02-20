f73e1301839a17bd6efecd14d2107688e4156a3e
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-02-20 10:04:06 -0300

    feat: add page and action for login

---
+++ src/actions/login.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+import bcrypt from 'bcryptjs'
+import { z } from 'zod'
+import jwt from 'jsonwebtoken'
+import { env } from '@/lib/env'
+import { cookies } from 'next/headers'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+type LoginProps = z.infer<typeof loginSchema>
+
+const login = async (data: LoginProps) => {
+  const { email, password } = loginSchema.parse(data)
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (!user) {
+    throw new Error('Usuário não encontrado')
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password)
+
+  if (!isValidPassword) {
+    throw new Error('Senha inválida')
+  }
+
+  const token = jwt.sign({ userId: user.id }, env.JWT_SECRET, {
+    expiresIn: '7d',
+  })
+
+  const cookieStore = await cookies()
+
+  cookieStore.set('token', token, {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  })
+
+  return true
+}
+
+export { login }
---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { login } from '@/actions/login'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Label } from '@radix-ui/react-dropdown-menu'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
+})
+
+type LoginProps = z.infer<typeof loginSchema>
+
+const LoginPage = () => {
+  const { push } = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const onSubmit = async (data: LoginProps) => {
+    await login(data)
+      .then(() => {
+        toast.success('Login realizado com sucesso')
+        push('/')
+      })
+      .catch(error => {
+        toast.error(`Erro ao realizar login: ${error.message}`)
+      })
+  }
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-2 border rounded-md max-w-xl w-full mx-auto p-4"
+      >
+        <p className="font-medium text-2xl">Login</p>
+        <div className="space-y-1">
+          <Label>E-mail</Label>
+          <Input type="email" {...register('email')} />
+          {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
+        </div>
+        <div className="space-y-1">
+          <Label>Senha</Label>
+          <Input type="password" {...register('password')} />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
+        </div>
+        <div className="flex items-center justify-end gap-2">
+          <Button
+            onClick={() => push('/registrar')}
+            type="button"
+            variant="secondary"
+          >
+            Registrar
+          </Button>
+          <Button type="submit">Entrar</Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
