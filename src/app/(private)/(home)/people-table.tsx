3b307e94843da1cb95768547a42958211ffc62cf
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-02-24 13:47:29 -0300

    feat: add actions column

--- src/app/(private)/(home)/people-table.tsx
+++ src/app/(private)/(home)/people-table.tsx
@@ -10,6 +10,9 @@ import {
 import type { Person } from '@prisma/client'
 import dayjs from 'dayjs'
 import { Pagination } from './pagination'
+import { Button } from '@/components/ui/button'
+import { Pen, Plus } from 'lucide-react'
+import { DeletePerson } from './delete-person'
 
 interface PeopleTableProps {
   people: Person[]
@@ -27,6 +30,7 @@ const PeopleTable = ({ people, count }: PeopleTableProps) => {
             <TableHead>CPF</TableHead>
             <TableHead>Telefone</TableHead>
             <TableHead>Data de Nascimento</TableHead>
+            <TableHead>Ações</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -40,14 +44,23 @@ const PeopleTable = ({ people, count }: PeopleTableProps) => {
                 <TableCell>
                   {dayjs(person.birthDate).format('DD/MM/YYYY')}
                 </TableCell>
+                <TableCell className="flex items-center gap-2">
+                  <Button size="icon" title="Ver mais">
+                    <Plus />
+                  </Button>
+                  <Button variant="secondary" size="icon" title="Editar">
+                    <Pen />
+                  </Button>
+                  <DeletePerson id={person.id} name={person.name} />
+                </TableCell>
               </TableRow>
             )
           })}
         </TableBody>
         <TableFooter>
           <TableRow>
-            <TableCell colSpan={5}>
-                <Pagination count={people.length} total={count} />
+            <TableCell colSpan={6}>
+              <Pagination count={people.length} total={count} />
             </TableCell>
           </TableRow>
         </TableFooter>
