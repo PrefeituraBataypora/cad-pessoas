015d2dbe95273d40ef6a9790c990d44364bc0428
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-02-24 11:50:41 -0300

    feat: implement data fetching and error handling in Home component

--- src/app/(private)/(home)/page.tsx
+++ src/app/(private)/(home)/page.tsx
@@ -1,9 +1,41 @@
+'use client'
+
+import { getPeople } from '@/actions/get-people'
+import { useQuery } from '@tanstack/react-query'
+import { PeopleTable } from './people-table'
+import { useSearchParams } from 'next/navigation'
+
 const Home = () => {
+  const searchParams = useSearchParams()
+  const page = Number(searchParams.get('pagina')) || 1
+  const limit = Number(searchParams.get('quantidade')) || 10
+
+  const { data, isFetching, isError, status } = useQuery({
+    queryKey: ['people', page, limit],
+    queryFn: async () => getPeople({ page, limit }),
+  })
+
+  if (isFetching) {
+    return (
+      <div className="w-full h-[calc(100vh-5rem)] flex items-center justify-center">
+        Carregando...
+      </div>
+    )
+  }
+
+  if (isError || !data) {
+    return (
+      <div className="w-full h-[calc(100vh-5rem)] flex items-center justify-center">
+        Ocorreu um erro ao carregar os dados
+      </div>
+    )
+  }
+
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="mt-2">
+      <PeopleTable people={data?.people || []} count={data?.total} />
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
