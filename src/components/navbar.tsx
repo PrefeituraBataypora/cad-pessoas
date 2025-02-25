1d9c06205e3a7bba550c99c709b2054ee35674d4
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-02-25 10:07:01 -0300

    feat: add CreatePerson component to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { CreatePerson } from '@/app/(private)/(home)/create.person'
 import { ChangeTheme } from './change-theme'
 import { UserButton } from './user-button'
 
@@ -5,7 +6,8 @@ const Navbar = () => {
   return (
     <div className="w-full border rounded-md p-2 flex items-center justify-between">
       <p className="text-xl font-medium">Cadastro de Pessoas</p>
-      <div className='flex items-center gap-2'>
+      <div className="flex items-center gap-2">
+        <CreatePerson />
         <ChangeTheme />
         <UserButton />
       </div>
